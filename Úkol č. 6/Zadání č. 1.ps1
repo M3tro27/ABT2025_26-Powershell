6e3449c332275b6d321c337b776632d7bc8aecae
M3tro27 <jakubvagner2002@gmail.com>
2026-01-11 15:32:53 +0100

    Revert "Řešení úkolu č. 6"
    This reverts commit d848c2d27c736262ffd433849c83d023d7f16c08.

--- Úkol č. 6/Zadání č. 1.ps1
+++
@@ -1,5 +0,0 @@
-for ($i = 0; $i -lt 10; $i++) {
-    $number = Get-Random -Minimum 10 -Maximum 100
-    $power = $number * $number
-    "{0, -5} {1, -10}" -f $number, $power
-}
--- Úkol č. 6/Zadání č. 2.ps1
+++
@@ -1,5 +0,0 @@
-$text = "Kobyla má malý bok"
-$array = $text.ToCharArray()
-$sorted_array = $array | Sort-Object
-$final_string = $sorted_array -Join ''
-$final_string
